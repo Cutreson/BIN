ed2346b8450521361271dbca5ca643ff40ada607
dinhhongsonpt <dinhhongsontbpt@gmail.com>
2021-11-06 19:34:34 +0700

    Commit

---
+++ OpenCV/03_NhanDien/NhanDien.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import os
+from PIL import Image
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+path = "F:/QT/BIN/OpenCV/02_Data/dataSet"
+
+def getImageWithID(path):
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+    #print(imagePaths)
+    faces = []
+    IDs = []
+    for imagePath in imagePaths:
+        faceImg = Image.open(imagePath).convert("L")
+        faceNp = np.array(faceImg,"uint8")
+        #print(faceNp)
+        Id = int(imagePath.split("\\")[1].split(".")[1])
+        faces.append(faceNp)
+        IDs.append(Id)
+        
+        cv2.imshow("trainning",faceNp)
+        cv2.waitKey(10)
+    return faces, IDs
+
+
+faces, IDs =  getImageWithID(path)
+recognizer.train(faces, np.array(IDs))
+if not os.path.exists("F:/QT/BIN/OpenCV/03_NhanDien/recoginizer"):
+    os.makedirs("F:/QT/BIN/OpenCV/03_NhanDien/recoginizer")
+recognizer.save("F:/QT/BIN/OpenCV/03_NhanDien/recoginizer/trainningData.yml")
+cv2.destroyAllWindows()
