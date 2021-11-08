f6eda83167f51962af28bf76016c725d705f2789
dinhhongsonpt <dinhhongsontbpt@gmail.com>
2021-11-08 13:22:51 +0700

    COmmit

--- Demo/Ver_1/GetData.py
+++ Demo/Ver_1/GetData.py
@@ -10,4 +10,11 @@ def insertIntoDatabase(SoTu,ThoiGianGuiDo,ThoiGianLayDo,TrangThai):
     conn.commit()
     conn.close() 
     ########################    
+def updateIntoDatabase(ID,ThoiGianLayDo,TrangThai):
+    conn = sqlite3.connect("F:/QT/BIN/Demo/Ver_1/database.db")
+    query = "UPDATE data SET ThoiGianLayDo = '" + str(ThoiGianLayDo) + "','" + str(TrangThai) + "' WHERE ID = " + str(ID)
+    conn.execute(query)
+    conn.commit()
+    conn.close() 
 insertIntoDatabase(1,"8h00","9h00","Da Gui")
+updateIntoDatabase(14,"15h00","Da Lay")
