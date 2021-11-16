863cd1fc029f0e1071db55ee940df0efb777d4ac
dinhhongsonpt <dinhhongsontbpt@gmail.com>
2021-11-17 01:29:48 +0700

    Commit 17/11

--- Demo/Ver_1/NhanDien.py
+++ Demo/Ver_1/NhanDien.py
@@ -41,3 +41,26 @@ def nhanDien():
 
 #####################################################
 nhanDien()
+
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+while(True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,1.3,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+        roi_gray = gray[y:y+h,x:x+w]
+        SoTu,confidence = recognizer.predict(roi_gray)
+        if confidence < 70:
+            profile = getProfile(SoTu)
+            if(profile != None):
+                cv2.putText(frame,""+str(profile[1]), (x+10,y+h+30), cv2.FONT_ITALIC, 1, (0,255,0),2)
+        else:
+            cv2.putText(frame,"Unknow", (x+10,y+h+30), cv2.FONT_ITALIC, 1, (0,0,255),2)
+    cv2.imshow("image",frame)
+    if(cv2.waitKey(1) == ord("q")):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
